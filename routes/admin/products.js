66a53c4bf2127c084b31fb05f4224c5da7c0c65f
trungkhanh2709 <111423000+trungkhanh2709@users.noreply.github.com>
2025-02-11 18:07:55 +0700

    change product

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -18,19 +18,29 @@ router.get('/all', async function (req, res, next) {
 });
 
 /* POST create a new product */
-router.post('/create', async function (req, res, next) {
+router.post("/create", async function (req, res, next) {
     try {
-        const newProductId = await generateId('PR');
-        const newProduct = new Product({
-            _id: newProductId,
-            ...req.body
-        });
-        await newProduct.save();
-        res.successResponse(newProduct, 'Product created successfully');
+      const newProductId = await generateId("PR");
+      const { name, description, category, brand, variations, images } = req.body;
+  
+      const newProduct = new Product({
+        _id: newProductId,
+        name,
+        description,
+        category,
+        brand,
+        variations, // Nhận danh sách loại hàng từ request body
+        images,
+        status: "active",
+      });
+  
+      await newProduct.save();
+      res.successResponse(newProduct, "Product created successfully");
     } catch (err) {
-        res.errorResponse('Failed to create product', 500, {}, { error: err.message });
+      res.errorResponse("Failed to create product", 500, {}, { error: err.message });
     }
-});
+  });
+  
 
 /* PUT update an existing product */
 router.put('/update/:id', async function (req, res, next) {
