f50ca705e34315d35e2cfcae461f2e078aeedfc0
trungkhanh2709 <111423000+trungkhanh2709@users.noreply.github.com>
2025-03-28 04:15:41 +0700

    backup

--- routes/clients/orderClient.js
+++ routes/clients/orderClient.js
@@ -1,53 +1,80 @@
-
 const express = require("express");
 const mongoose = require("mongoose");
 const Order = require("../../models/orders");
 const User = require("../../models/users"); // Đảm bảo đường dẫn đúng
-const generateId = require('../../utils/generateId');
-const {authenticate} = require("../../middlewares/auth")
+const generateId = require("../../utils/generateId");
+const { authenticate } = require("../../middlewares/auth");
 const Notification = require("../../models/Notification");
 const Employee = require("../../models/employees");
 const router = express.Router();
 const { db, admin } = require("../../config/firebase"); // Import Firestore
+const formatCurrency = require("../../utils/formatCurrency"); // Import hàm formatCurrency
 
-
-
-async function saveNotificationToFirestore(employeeId, title, message) {
+async function saveNotificationToFirestore(
+  employeeId,
+  title,
+  message,
+  orderId
+) {
   try {
     await db.collection("notifications").add({
       employee_id: employeeId,
+      orderId,
       title,
       message,
       timestamp: admin.firestore.FieldValue.serverTimestamp(), // Thời gian thực
     });
-    console.log("✅ Lưu thông báo vào Firestore thành công!");
+    console.log("Lưu thông báo vào Firestore thành công!");
   } catch (error) {
-    console.error("❌ Lỗi khi lưu thông báo vào Firestore:", error);
+    console.error("Lỗi khi lưu thông báo vào Firestore:", error);
   }
 }
 
 // API tạo đơn hàng
 router.post("/create", authenticate, async (req, res) => {
   try {
-    const { 
-      user_id, 
-      totalPrice, 
-      status, 
+    const {
+      user_id,
+      totalPrice,
+      status,
       deliveryAddress,
       createdAt,
       updatedAt,
-      items 
+      items,
     } = req.body;
 
     // Kiểm tra thông tin bắt buộc
-    if (!user_id || !totalPrice || !status || !deliveryAddress || !items || !Array.isArray(items) || items.length === 0) {
-      return res.status(400).json({ message: "Thiếu thông tin đơn hàng hoặc danh sách sản phẩm không hợp lệ" });
+    if (
+      !user_id ||
+      !totalPrice ||
+      !status ||
+      !deliveryAddress ||
+      !items ||
+      !Array.isArray(items) ||
+      items.length === 0
+    ) {
+      return res
+        .status(400)
+        .json({
+          message:
+            "Thiếu thông tin đơn hàng hoặc danh sách sản phẩm không hợp lệ",
+        });
     }
 
     // Kiểm tra các trường trong deliveryAddress
-    const { provinceName, districtName, wardName, street, name, phone } = deliveryAddress;
-    if (!provinceName || !districtName || !wardName || !street || !name || !phone) {
-      return res.status(400).json({ message: "Thiếu thông tin địa chỉ giao hàng" });
+    const { provinceName, districtName, wardName, street, name, phone } =
+      deliveryAddress;
+    if (
+      !provinceName ||
+      !districtName ||
+      !wardName ||
+      !street ||
+      !name ||
+      !phone
+    ) {
+      return res
+        .status(400)
+        .json({ message: "Thiếu thông tin địa chỉ giao hàng" });
     }
 
     // Tạo ID duy nhất cho đơn hàng
@@ -61,7 +88,12 @@ router.post("/create", authenticate, async (req, res) => {
       status,
       deliveryPhoneNumber: phone,
       deliveryName: name,
-      deliveryAdress: { province: provinceName, district: districtName, ward: wardName, street },
+      deliveryAdress: {
+        province: provinceName,
+        district: districtName,
+        ward: wardName,
+        street,
+      },
       items,
       createdAt: createdAt || Date.now(),
       updatedAt: updatedAt || Date.now(),
@@ -69,84 +101,105 @@ router.post("/create", authenticate, async (req, res) => {
 
     // Lưu vào database
     await newOrder.save();
-   
+
     const employees = await Employee.find({ role: "admin" });
     console.log("🔎 Danh sách admin:", employees);
     for (const employee of employees) {
-      await saveNotificationToFirestore(employee._id, "Đơn hàng mới", `Có đơn hàng mới trị giá ${totalPrice} VND`);
+      await saveNotificationToFirestore(
+        employee._id,
+        "Đơn hàng mới",
+        `Có đơn hàng mới trị giá ${formatCurrency(totalPrice)} VND`, 
+        orderId
+      );
     }
 
-
     // 🔥 Gửi thông báo FCM
-    const employeeTokens = employees.map(e => e.fcmToken).filter(Boolean);
+    const employeeTokens = employees.map((e) => e.fcmToken).filter(Boolean);
     if (employeeTokens.length > 0) {
       const message = {
         notification: {
           title: "Đơn hàng mới!",
           body: `Có đơn hàng mới với tổng giá trị ${totalPrice} VND`,
+          orderId: orderId,
         },
-        tokens: employeeTokens
+        tokens: employeeTokens,
       };
 
-      admin.messaging().sendMulticast(message)
-        .then(response => console.log("✅ Gửi thông báo FCM thành công:", response))
-        .catch(error => console.error("❌ Lỗi khi gửi FCM:", error));
+      admin
+        .messaging()
+        .sendMulticast(message)
+        .then((response) =>
+          console.log("✅ Gửi thông báo FCM thành công:", response)
+        )
+        .catch((error) => console.error("❌ Lỗi khi gửi FCM:", error));
     }
 
-    return res.status(201).json({ message: "Tạo đơn hàng thành công", order: newOrder });
+    return res
+      .status(201)
+      .json({ message: "Tạo đơn hàng thành công", order: newOrder });
   } catch (error) {
-    return res.status(500).json({ message: "Lỗi khi tạo đơn hàng", error: error.message });
+    return res
+      .status(500)
+      .json({ message: "Lỗi khi tạo đơn hàng", error: error.message });
   }
 });
 
-  
 router.put("/update-status", async (req, res) => {
-    const { orderId, status } = req.body;
-  
-    if (!orderId || !status) {
-      return res.status(400).json({ message: "Thiếu thông tin orderId hoặc status!" });
+  const { orderId, status } = req.body;
+
+  if (!orderId || !status) {
+    return res
+      .status(400)
+      .json({ message: "Thiếu thông tin orderId hoặc status!" });
+  }
+
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(
+      orderId,
+      { status },
+      { new: true }
+    );
+
+    if (!updatedOrder) {
+      return res.status(404).json({ message: "Không tìm thấy đơn hàng!" });
     }
-  
-    try {
-      const updatedOrder = await Order.findByIdAndUpdate(
-        orderId,
-        { status },
-        { new: true }
-      );
-  
-      if (!updatedOrder) {
-        return res.status(404).json({ message: "Không tìm thấy đơn hàng!" });
-      }
-  
-      return res.json({ message: "Cập nhật trạng thái thành công!", order: updatedOrder });
-    } catch (error) {
-      console.error("Lỗi cập nhật trạng thái đơn hàng:", error);
-      return res.status(500).json({ message: "Lỗi server khi cập nhật trạng thái đơn hàng." });
-    }
-  });
-  
+
+    return res.json({
+      message: "Cập nhật trạng thái thành công!",
+      order: updatedOrder,
+    });
+  } catch (error) {
+    console.error("Lỗi cập nhật trạng thái đơn hàng:", error);
+    return res
+      .status(500)
+      .json({ message: "Lỗi server khi cập nhật trạng thái đơn hàng." });
+  }
+});
+
 router.put("/cancel/:orderId", authenticate, async (req, res) => {
-    try {
-        const { orderId } = req.params;
+  try {
+    const { orderId } = req.params;
 
-        // Kiểm tra đơn hàng có tồn tại không
-        const order = await Order.findById(orderId);
-        if (!order) {
-            return res.status(404).json({ message: "Không tìm thấy đơn hàng" });
-        }
-
-        // Kiểm tra trạng thái đơn hàng, nếu đã thanh toán thì không hủy
-        if (order.status !== "Chờ xác nhận") {
-            return res.status(400).json({ message: "Đơn hàng không thể bị hủy" });
-        }
-
-        // Cập nhật trạng thái đơn hàng thành "Đã hủy"
-        order.status = "Đã hủy";
-        await order.save();
-
-        return res.status(200).json({ message: "Đơn hàng đã bị hủy", order });
-    } catch (error) {
-        return res.status(500).json({ message: "Lỗi khi hủy đơn hàng", error: error.message });
+    // Kiểm tra đơn hàng có tồn tại không
+    const order = await Order.findById(orderId);
+    if (!order) {
+      return res.status(404).json({ message: "Không tìm thấy đơn hàng" });
     }
+
+    // Kiểm tra trạng thái đơn hàng, nếu đã thanh toán thì không hủy
+    if (order.status !== "Chờ xác nhận") {
+      return res.status(400).json({ message: "Đơn hàng không thể bị hủy" });
+    }
+
+    // Cập nhật trạng thái đơn hàng thành "Đã hủy"
+    order.status = "Đã hủy";
+    await order.save();
+
+    return res.status(200).json({ message: "Đơn hàng đã bị hủy", order });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Lỗi khi hủy đơn hàng", error: error.message });
+  }
 });
 module.exports = router;
