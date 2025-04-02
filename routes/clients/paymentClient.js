0463da90d7eadd5cb166b57665b2f682d8aff09c
trungkhanh2709 <111423000+trungkhanh2709@users.noreply.github.com>
2025-04-02 20:17:39 +0700

    fix granted noti

--- routes/clients/paymentClient.js
+++ routes/clients/paymentClient.js
@@ -27,7 +27,7 @@ router.post("/create", async (req, res) => {
     if (!order) {
       return res.status(404).json({ message: "Không tìm thấy đơn hàng!" });
     }
-    const newPaymentId = await generateId('PAY');
+    const newPaymentId = await generateId('PA');
 
     // Tạo payment mới
     const newPayment = new Payment({
