dbc027c6532569968cedf6d1d1b09077a1c04b83
Sainoo <nvtrung19.work@gmail.com>
2025-05-04 14:51:45 +0700

    GymZ Release Version1.5

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 
 app.use(cors({
-  origin: URL_FRONTEND,
+  origin: 'https://gym-z-mu.vercel.app',
   methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
   allowedHeaders: 'Content-Type, Authorization, cache-control',
   credentials: true
