d72e63fc8c01e8bc578cfa55ffda81c78882997d
trungkhanh2709 <111423000+trungkhanh2709@users.noreply.github.com>
2025-05-08 04:39:19 +0700

    Update MomoPayment.js

--- routes/API_Third_Party/payment/MomoPayment.js
+++ routes/API_Third_Party/payment/MomoPayment.js
@@ -115,7 +115,7 @@ router.get("/callback", async (req, res) => {
     if (resultCode === "0") {
 
       try {
-        const response = await axios.post(`${URL_API}paymentClient/create`, {
+        const response = await axios.post(`${URL_BACKEND}/paymentClient/create`, {
           orderId: finalOrderId,
           paymentMethod: selectedMethod ,
         });
